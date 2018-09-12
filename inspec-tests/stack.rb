4f640c02f6717d7445d3242fda61a68279b79dce
Vru <go4vru@gmail.com>
2018-09-12 12:57:59 +0530

    Create stack.rb

---
+++ inspec-tests/stack.rb
@@ -0,0 +1,16 @@
+describe service('tomcat7') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe service('mysql') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port(8080) do
+  it { should be_listening }
+  its('processes') {should include 'java'}
+end
